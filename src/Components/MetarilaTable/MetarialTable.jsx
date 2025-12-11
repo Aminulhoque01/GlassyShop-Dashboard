b35dbf23c9e6febf078478e11f8a5babff526661
Aminulhoque01 <aminuhoque53@gmail.com>
2025-12-11 21:35:05 +0600

    metarial ui

--- src/Components/MetarilaTable/MetarialTable.jsx
+++ src/Components/MetarilaTable/MetarialTable.jsx
@@ -1,3 +1,4 @@
+import Checkbox from "@mui/material/Checkbox";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -55,23 +56,7 @@ function createData(name, code, population, size) {
   return { name, code, population, size, density };
 }
 
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-];
+ 
 
 const MetarialTable = () => {
   const [page, setPage] =  useState(0);
@@ -85,7 +70,7 @@ const MetarialTable = () => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-
+const label = { slotProps: { input: { "aria-label": "Checkbox demo" } } };
   return (
     <>
       <Paper sx={{ width: "100%", overflow: "hidden" }}>
@@ -105,36 +90,42 @@ const MetarialTable = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row.code}
-                    >
-                      {columns.map((column) => {
-                        const value = row[column.id];
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
+               <TableRow key={0}>
+                 <TableCell style={{minWidth:columns.minWidth}}>
+                  <Checkbox {...label} size="small"/>
+                 </TableCell>
+                 <TableCell style={{minWidth:columns.minWidth}}>
+                  fgf
+                 </TableCell>
+                 <TableCell style={{minWidth:columns.minWidth}}>
+                dfd
+                 </TableCell>
+                 <TableCell style={{minWidth:columns.minWidth}}>
+                  &nbsp;
+                 </TableCell>
+                 <TableCell style={{minWidth:columns.minWidth}}>
+                  &nbsp;
+                 </TableCell>
+                 <TableCell style={{minWidth:columns.minWidth}}>
+                  &nbsp;
+                 </TableCell>
+                 <TableCell style={{minWidth:columns.minWidth}}>
+                  &nbsp;
+                 </TableCell>
+                 <TableCell style={{minWidth:columns.minWidth}}>
+                  &nbsp;
+                 </TableCell>
+                 <TableCell style={{minWidth:columns.minWidth}}>
+                  &nbsp;
+                 </TableCell>
+               </TableRow>
             </TableBody>
           </Table>
         </TableContainer>
         <TablePagination
           rowsPerPageOptions={[10, 25, 100]}
           component="div"
-          count={rows.length}
+           
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
