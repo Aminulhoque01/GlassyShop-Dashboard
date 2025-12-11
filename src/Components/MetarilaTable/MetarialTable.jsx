3a841c06d40e18cfc5e208d918bdce6ca7be936e
Aminulhoque01 <aminuhoque53@gmail.com>
2025-12-11 22:43:18 +0600

    product

--- src/Components/MetarilaTable/MetarialTable.jsx
+++ src/Components/MetarilaTable/MetarialTable.jsx
@@ -8,6 +8,7 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import { useState } from "react";
+import { Link } from "react-router";
 
 const columns = [
   { id: "id", label: "ID", minWidth: 80 },
@@ -17,50 +18,42 @@ const columns = [
     id: "subcategory",
     label: "Sub Category",
     minWidth: 170,
-    
   },
   {
     id: "brand",
     label: "Brand",
     minWidth: 170,
-    
   },
   {
     id: "price",
     label: "Price",
     minWidth: 130,
-    
   },
   {
     id: "sales",
     label: "Sales",
     minWidth: 100,
-    
   },
   {
     id: "rating",
     label: "Rating",
     minWidth: 80,
-    
   },
   {
     id: "action",
     label: "Action",
     minWidth: 120,
-    
   },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
- 
+// function createData(name, code, population, size) {
+//   const density = population / size;
+//   return { name, code, population, size, density };
+// }
 
 const MetarialTable = () => {
-  const [page, setPage] =  useState(0);
-  const [rowsPerPage, setRowsPerPage] =  useState(10);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -70,7 +63,7 @@ const MetarialTable = () => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-const label = { slotProps: { input: { "aria-label": "Checkbox demo" } } };
+  const label = { slotProps: { input: { "aria-label": "Checkbox demo" } } };
   return (
     <>
       <Paper sx={{ width: "100%", overflow: "hidden" }}>
@@ -78,6 +71,9 @@ const label = { slotProps: { input: { "aria-label": "Checkbox demo" } } };
           <Table stickyHeader aria-label="sticky table">
             <TableHead>
               <TableRow>
+                <TableCell>
+                  <Checkbox {...label} size="" />
+                </TableCell>
                 {columns.map((column) => (
                   <TableCell
                     key={column.id}
@@ -90,42 +86,67 @@ const label = { slotProps: { input: { "aria-label": "Checkbox demo" } } };
               </TableRow>
             </TableHead>
             <TableBody>
-               <TableRow key={0}>
-                 <TableCell style={{minWidth:columns.minWidth}}>
-                  <Checkbox {...label} size="small"/>
-                 </TableCell>
-                 <TableCell style={{minWidth:columns.minWidth}}>
-                  fgf
-                 </TableCell>
-                 <TableCell style={{minWidth:columns.minWidth}}>
-                dfd
-                 </TableCell>
-                 <TableCell style={{minWidth:columns.minWidth}}>
+              <TableRow key={0}>
+                <TableCell style={{ minWidth: columns.minWidth }}>
+                  <Checkbox {...label} size="" />
+                </TableCell>
+                <TableCell style={{ minWidth: columns.minWidth }}>
+                  <div className="flex items-center gap-4 w-[300px]">
+                    <div className="img w-[65px] h-[65px] rounded-md overflow-hidden group">
+                      <Link to="/product/325345" data-discover="true">
+                        <img
+                          src="https://isomorphic-furyroad.vercel.app/_next/image?url=https%3A%2F%2Fisomorphic-furyroad.s3.amazonaws.com%2Fpublic%2Fcategories%2Fbags.webp&w=1920&q=75"
+                          alt=""
+                          className="w-full group-hover:scale-105 transition-all"
+                        />
+                      </Link>
+                    </div>
+                    <div className="info w-[75%]">
+                      <h3 className="font-[600] text-[12px] leading-4 hover:text-blue-500">
+                        <Link to="/product/4656">
+                          VNEED Women Embroidered Rayon Kurta Pant set | kurta
+                          set for Women
+                        </Link>
+                      </h3>
+                      <span className="text-[12px]">Books</span>
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell style={{ minWidth: columns.minWidth }}>
+                  Electronics
+                </TableCell>
+                <TableCell style={{ minWidth: columns.minWidth }}>
+                  Woman
+                </TableCell>
+                <TableCell style={{ minWidth: columns.minWidth }}>
+                  <div className="flex items-center gap-1 flex-col">
+                    <span className="oldPrice line-through text-gray-500 text-[15px] font-[500]">
+                      $58.00
+                    </span>
+                    <span className="  text-blue-600 text-[15px] font-[600]">
+                      $58.00
+                    </span>
+                  </div>
+                </TableCell>
+                <TableCell style={{ minWidth: columns.minWidth }}>
                   &nbsp;
-                 </TableCell>
-                 <TableCell style={{minWidth:columns.minWidth}}>
+                </TableCell>
+                <TableCell style={{ minWidth: columns.minWidth }}>
                   &nbsp;
-                 </TableCell>
-                 <TableCell style={{minWidth:columns.minWidth}}>
+                </TableCell>
+                <TableCell style={{ minWidth: columns.minWidth }}>
                   &nbsp;
-                 </TableCell>
-                 <TableCell style={{minWidth:columns.minWidth}}>
+                </TableCell>
+                <TableCell style={{ minWidth: columns.minWidth }}>
                   &nbsp;
-                 </TableCell>
-                 <TableCell style={{minWidth:columns.minWidth}}>
-                  &nbsp;
-                 </TableCell>
-                 <TableCell style={{minWidth:columns.minWidth}}>
-                  &nbsp;
-                 </TableCell>
-               </TableRow>
+                </TableCell>
+              </TableRow>
             </TableBody>
           </Table>
         </TableContainer>
         <TablePagination
           rowsPerPageOptions={[10, 25, 100]}
           component="div"
-           
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
