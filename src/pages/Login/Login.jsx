71a0d32fb8316ab0e6e6a9364540a230a46fdf35
Aminulhoque01 <aminuhoque53@gmail.com>
2025-12-22 20:56:49 +0600

    add google login and facebook loadin

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,25 @@
 import { Link, NavLink } from "react-router";
-import image1 from "../../assets/background.avif";
-import logo from "../../assets/logo-light-full.png";
 
+import logo from "../../assets/logo-light-full.png";
+import login from "../../assets/loginlogo.svg";
 import { IoIosLogIn } from "react-icons/io";
 import { FaRegUserCircle } from "react-icons/fa";
 import Button from "@mui/material/Button";
+import { useState } from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
 
 const Login = () => {
+  const [loadingGoogle, setLoadingGoogle] = useState(false);
+  const [loadingFacebook, setLoadingFacebook] = useState(false);
+  function handleClickGoogle() {
+    setLoadingGoogle(true);
+  }
+  function handleClickFacebook() {
+    setLoadingFacebook(true);
+  }
+
   return (
     <section className="fixed top-0 left-0 w-full h-full bg-white">
       <header className="w-full fixed top-0 left-0 px-4 py-2 flex items-center justify-between">
@@ -21,13 +34,47 @@ const Login = () => {
             </Button>
           </NavLink>
           <NavLink to="/signup" exact={true} activeClassName="isActive">
-          <Button className="!cursor-pointer flex gap-2 !rounded-full !text-[rgba(0,0,0,0.8)] !px-5  ">
-            <FaRegUserCircle className="flex gap-2" /> Sign up
-          </Button>
+            <Button className="!cursor-pointer flex gap-2 !rounded-full !text-[rgba(0,0,0,0.8)] !px-5  ">
+              <FaRegUserCircle className="flex gap-2" /> Sign up
+            </Button>
           </NavLink>
         </div>
       </header>
-      <img src={image1} alt="" className="w-full top-0 left-0 opacity-5" />
+
+      <div className="loginBox card w-[45%] h-[300px] mx-auto mt-20 relative z-50">
+        <div className="text-center  ">
+          <img src={login} alt="" className="m-auto " />
+        </div>
+        <h1 className="text-center text-[35px] font-[800] mt-4">
+          Welcome Back ! <br />
+          Sing in with your credentials
+        </h1>
+
+        <div className="flex items-center justify-center w-full mt-5 gap-4">
+          <LoadingButton
+            size="small"
+            onClick={handleClickGoogle}
+            endIcon={<FcGoogle className="!text-[30px]" />}
+            loading={loadingGoogle}
+            loadingPosition="end"
+            variant="outlined"
+            className="!bg-none !text-[15px] !capitalize !px-5 !font-[600] !text-[rgba(0,0,0,0.7)] !border-[rgba(0,0,0,0.5)]"
+          >
+            Sing With Google
+          </LoadingButton>
+          <LoadingButton
+            size="small"
+            onClick={handleClickFacebook}
+            endIcon={<FaFacebook className="!text-[30px]" />}
+            loading={loadingFacebook}
+            loadingPosition="end"
+            variant="outlined"
+            className="!bg-none !text-[15px] !capitalize !px-5 !font-[600] !text-[rgba(0,0,0,0.7)] !border-[rgba(0,0,0,0.5)]"
+          >
+            Sing With Facebook
+          </LoadingButton>
+        </div>
+      </div>
     </section>
   );
 };
