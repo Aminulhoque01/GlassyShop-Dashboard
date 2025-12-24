da1ca63058a34461a71c83d7bfb44d4f8e0addce
Aminulhoque01 <aminuhoque53@gmail.com>
2025-12-24 21:48:34 +0600

    add price fileds and category and old

--- src/pages/AddProducts/AddProduct.jsx
+++ src/pages/AddProducts/AddProduct.jsx
@@ -7,9 +7,13 @@ import { useState } from "react";
 
 const AddProduct = () => {
   const [productCate, setProductCat] = useState("");
+  const [productSubCate, setProductSubCat] = useState("");
 
   const handleChangeProductCate = (event) => {
     setProductCat(event.target.value);
+  };
+  const handleChangeProductSubCate = (event) => {
+    setProductSubCat(event.target.value);
   };
 
   return (
@@ -17,38 +21,75 @@ const AddProduct = () => {
       <form>
         <div className="grid grid-cols-1 mb-3">
           <div className="col">
-            <h3>Product Name</h3>
+            <h3 className="text-[14px] font-[500] mb-1 text-black">Product Name</h3>
             <input
               type="text"
-              className="w-full h-[40px] border border-[rgba(0,0,0,0.1)] focus:outline-none
+              className="w-full h-[40px] border border-[rgba(0,0,0,0.2)] bg-[#f3f3f3] focus:outline-none
                      focus:border-[rgba(0,0,0,0.4)] rounded-sm p-3 text-sm"
             />
           </div>
         </div>
         <div className="grid grid-cols-1 mb-3">
           <div className="col">
-            <h3>Product Description</h3>
+            <h3  className="text-[14px] font-[500] mb-1 text-black">Product Description</h3>
             <textarea
               type="text"
-              className="w-full h-[140px] border border-[rgba(0,0,0,0.1)] focus:outline-none
+              className="w-full h-[140px] border border-[rgba(0,0,0,0.2)] bg-[#f3f3f3] focus:outline-none
                      focus:border-[rgba(0,0,0,0.4)] rounded-sm p-3 text-sm"
             />
           </div>
         </div>
-        <div className="grid grid-cols-4 mb-3">
+        <div className="grid grid-cols-4 mb-3 gap-5">
           <div className="col">
-            <h3>Product Category</h3>
+            <h3  className="text-[14px] font-[500] mb-1 text-black">Product Category</h3>
             <Select
               labelId="demo-simple-select-label"
-              id="demo-simple-select"
+              id="productCategoryDropDown"
+              size="small"
+              className="w-full bg-[#f3f3f3]"
+              value={productSubCate}
+              label="Category"
+              onChange={handleChangeProductSubCate}
+            >
+              <MenuItem value={null}>none</MenuItem>
+              <MenuItem value={10}>Fashion</MenuItem>
+              <MenuItem value={20}>Beauty</MenuItem>
+              <MenuItem value={30}>Wellness</MenuItem>
+            </Select>
+          </div>
+          <div className="col">
+            <h3 className="text-[14px] font-[500] mb-1 text-black">Product Sub Category</h3>
+            <Select
+              labelId="demo-simple-select-label"
+              id="productCategoryDropDown"
+              size="small"
+              className="w-full bg-[#f3f3f3]"
               value={productCate}
-              label="Age"
+              label="Category"
               onChange={handleChangeProductCate}
             >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+              <MenuItem value={null}>none</MenuItem>
+              <MenuItem value={10}>Fashion</MenuItem>
+              <MenuItem value={20}>Beauty</MenuItem>
+              <MenuItem value={30}>Wellness</MenuItem>
             </Select>
+          </div>
+
+          <div className="col">
+            <h3 className="text-[14px] font-[500] mb-1 text-black">Product Price</h3>
+            <input
+              type="number"
+              className="w-full h-[40px] border border-[rgba(0,0,0,0.2)] bg-[#f3f3f3] focus:outline-none
+                     focus:border-[rgba(0,0,0,0.4)] rounded-sm p-3 text-sm"
+            />
+          </div>
+          <div className="col">
+            <h3 className="text-[14px] font-[500] mb-1 text-black">Product Old Price</h3>
+            <input
+              type="number"
+              className="w-full h-[40px] border border-[rgba(0,0,0,0.2)] bg-[#f3f3f3] focus:outline-none
+                     focus:border-[rgba(0,0,0,0.4)] rounded-sm p-3 text-sm"
+            />
           </div>
         </div>
       </form>
