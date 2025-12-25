bc3702ea0166f975f93c1b5c8b22d61eed4b520b
Aminulhoque01 <aminuhoque53@gmail.com>
2025-12-25 22:12:33 +0600

    upload modal done

--- src/pages/AddProducts/AddProduct.jsx
+++ src/pages/AddProducts/AddProduct.jsx
@@ -10,6 +10,8 @@ import UploadBox from "../../Components/UploadBox/UploadBox";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { IoMdClose } from "react-icons/io";
+import Button from "@mui/material/Button";
+import { FaCloudUploadAlt } from "react-icons/fa";
 
 const AddProduct = () => {
   const [productCate, setProductCat] = useState("");
@@ -40,7 +42,8 @@ const AddProduct = () => {
 
   return (
     <section className="p-5 bg-gray-50">
-      <form className="p-8 py-3">
+      <form className="p-8 py-3 ">
+        <div className="max-h-[72vh] overflow-y-scroll pr-4">
         <div className="grid grid-cols-1 mb-3">
           <div className="col">
             <h3 className="text-[14px] font-[500] mb-1 text-black">
@@ -249,6 +252,68 @@ const AddProduct = () => {
             <div className="uploadBoxWrapper relative">
               <span className="absolute w-[20px] h-[20px] rounded-full overflow-hidden -top-[10px] -right-[10px] bg-red-500 flex items-center justify-center z-50 cursor-pointer">
                 <IoMdClose className="text-[17px] text-white"/></span>
+
+              <div
+                className="uploadBox p-3 rounded-md overflow-hidden border border-dashed 
+                  border-[rgba(0,0,0,0.3)] h-[150px] w-[100%] bg-gray-100 cursor-pointer hover:bg-gray-200
+                  flex items-center justify-center flex-col relative"
+              >
+                <LazyLoadImage
+                  alt={"image"}
+                  src="https://isomorphic-furyroad.vercel.app/_next/image?url=https%3A%2F%2Fisomorphic-furyroad.s3.amazonaws.com%2Fpublic%2Fproducts%2Fmodern%2F1.webp&w=1920&q=75" // use normal <img> attributes as props
+                  className="w-full h-full object-cover"
+                  effect="blur"
+                  wrapperProps={{
+                    style: { transitionDelay: "1s" },
+                  }}
+                />
+              </div>
+            </div>
+
+             <div className="uploadBoxWrapper relative">
+              <span className="absolute w-[20px] h-[20px] rounded-full overflow-hidden -top-[10px] -right-[10px] bg-red-500 flex items-center justify-center z-50 cursor-pointer">
+                <IoMdClose className="text-[17px] text-white"/></span>
+                
+              <div
+                className="uploadBox p-3 rounded-md overflow-hidden border border-dashed 
+                  border-[rgba(0,0,0,0.3)] h-[150px] w-[100%] bg-gray-100 cursor-pointer hover:bg-gray-200
+                  flex items-center justify-center flex-col relative"
+              >
+                <LazyLoadImage
+                  alt={"image"}
+                  src="https://isomorphic-furyroad.vercel.app/_next/image?url=https%3A%2F%2Fisomorphic-furyroad.s3.amazonaws.com%2Fpublic%2Fproducts%2Fmodern%2F1.webp&w=1920&q=75" // use normal <img> attributes as props
+                  className="w-full h-full object-cover"
+                  effect="blur"
+                  wrapperProps={{
+                    style: { transitionDelay: "1s" },
+                  }}
+                />
+              </div>
+            </div>
+             <div className="uploadBoxWrapper relative">
+              <span className="absolute w-[20px] h-[20px] rounded-full overflow-hidden -top-[10px] -right-[10px] bg-red-500 flex items-center justify-center z-50 cursor-pointer">
+                <IoMdClose className="text-[17px] text-white"/></span>
+                
+              <div
+                className="uploadBox p-3 rounded-md overflow-hidden border border-dashed 
+                  border-[rgba(0,0,0,0.3)] h-[150px] w-[100%] bg-gray-100 cursor-pointer hover:bg-gray-200
+                  flex items-center justify-center flex-col relative"
+              >
+                <LazyLoadImage
+                  alt={"image"}
+                  src="https://isomorphic-furyroad.vercel.app/_next/image?url=https%3A%2F%2Fisomorphic-furyroad.s3.amazonaws.com%2Fpublic%2Fproducts%2Fmodern%2F1.webp&w=1920&q=75" // use normal <img> attributes as props
+                  className="w-full h-full object-cover"
+                  effect="blur"
+                  wrapperProps={{
+                    style: { transitionDelay: "1s" },
+                  }}
+                />
+              </div>
+            </div>
+             <div className="uploadBoxWrapper relative">
+              <span className="absolute w-[20px] h-[20px] rounded-full overflow-hidden -top-[10px] -right-[10px] bg-red-500 flex items-center justify-center z-50 cursor-pointer">
+                <IoMdClose className="text-[17px] text-white"/></span>
+                
               <div
                 className="uploadBox p-3 rounded-md overflow-hidden border border-dashed 
                   border-[rgba(0,0,0,0.3)] h-[150px] w-[100%] bg-gray-100 cursor-pointer hover:bg-gray-200
@@ -267,6 +332,14 @@ const AddProduct = () => {
             </div>
           </div>
         </div>
+        
+        
+        </div>     
+           <hr />       
+        <br />
+        <Button type="submit" className="w-full btn-blue btn-lg flex items-center gap-2">
+          <FaCloudUploadAlt className="text-[25px] text-white"/>
+Published and View</Button>     
       </form>
     </section>
   );
