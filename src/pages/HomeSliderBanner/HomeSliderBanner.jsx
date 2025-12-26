1827121c1d9a60de29134f7852681a533ac9fbd5
Aminulhoque01 <aminuhoque53@gmail.com>
2025-12-26 23:23:39 +0600

    add home slider modaql

--- src/pages/HomeSliderBanner/HomeSliderBanner.jsx
+++ src/pages/HomeSliderBanner/HomeSliderBanner.jsx
@@ -7,26 +7,23 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router";
- 
+
 import Button from "@mui/material/Button";
 import { CiEdit } from "react-icons/ci";
 import { MdDeleteOutline, MdOutlineRemoveRedEye } from "react-icons/md";
+import { MyContext } from "../../App";
 
 const columns = [
-  
   { id: "Image", label: "Image", minWidth: 170 },
-   
-  
+
   {
     id: "action",
     label: "Action",
     minWidth: 120,
   },
 ];
-
-
 
 const HomeSliderBanner = () => {
   const [page, setPage] = useState(0);
@@ -41,22 +38,42 @@ const HomeSliderBanner = () => {
     setPage(0);
   };
   const label = { slotProps: { input: { "aria-label": "Checkbox demo" } } };
+
+  const context = useContext(MyContext);
   return (
     <>
+      <div className="flex items-center justify-between px-5 py-5 ">
+        <h2 className="text-[18spx] font-[600]">Home Slider Banner</h2>
+
+        <div className="col w-[25%] ml-auto flex items-center gap-3">
+          <Button className="btn   !text-white   "> Export</Button>
+          <Button
+            className="btn-blue   !text-white "
+            onClick={() =>
+              context.setIsOpenFullScreenPanel({
+                open: true,
+                model: "Add Home Slider",
+              })
+            }
+          >
+            Add Home Slider 
+          </Button>
+        </div>
+      </div>
       <Paper sx={{ width: "100%", overflow: "hidden" }}>
-        <TableContainer sx={{ maxHeight: 440 }} >
+        <TableContainer sx={{ maxHeight: 440 }}>
           <Table stickyHeader aria-label="sticky table">
-            <TableHead >
-              <TableRow >
+            <TableHead>
+              <TableRow>
                 <TableCell className="!bg-[#00369b] " width={60}>
-                  <Checkbox {...label} size="" className="!text-white"/>
+                  <Checkbox {...label} size="" className="!text-white" />
                 </TableCell>
                 {columns.map((column) => (
-                  <TableCell className="!bg-[#00369b] !text-white"
-                    width={300}
+                  <TableCell
+                    className="!bg-[#00369b] !text-white"
+                    width={column.minWidth}
                     key={column.id}
                     align={column.align}
-                    style={{ minWidth: column.minWidth }}
                   >
                     {column.label}
                   </TableCell>
@@ -65,12 +82,12 @@ const HomeSliderBanner = () => {
             </TableHead>
             <TableBody>
               <TableRow key={0}>
-                <TableCell style={{ minWidth: columns.minWidth }}>
+                <TableCell width={100}>
                   <Checkbox {...label} size="" />
                 </TableCell>
-                <TableCell style={{ minWidth: columns.minWidth }}>
+                <TableCell width={100}>
                   <div className="flex items-center gap-4 w-[300px]">
-                    <div className="img w-full   rounded-md overflow-hidden group">
+                    <div className="img w-full h-[80px]  rounded-md overflow-hidden group">
                       <Link to="/product/325345" data-discover="true">
                         <img
                           src="https://isomorphic-furyroad.vercel.app/_next/image?url=https%3A%2F%2Fisomorphic-furyroad.s3.amazonaws.com%2Fpublic%2Fcategories%2Fbags.webp&w=1920&q=75"
@@ -79,32 +96,34 @@ const HomeSliderBanner = () => {
                         />
                       </Link>
                     </div>
-                    
                   </div>
                 </TableCell>
-               
-                 
-                
-                <TableCell style={{ minWidth: columns.minWidth }}>
+
+                <TableCell width={100}>
                   <div className="flex items-center gap-1">
-                            <Button className="!w-[35px] !h-[35px] !min-w-[35px] bg-[#f1f1f1]
-                             !border border-[rgba(0,0,0,0.4)] !rounded-full hover:!bg-[#f1f1f1]" title="Edit">
-                              <CiEdit className="text-[rgba(0,0,0,0.7)] text-[20px]"/>
-                            </Button>
-                            <Button className="!w-[35px] !h-[35px] !min-w-[35px] bg-[#f1f1f1] !rounded-full hover:!bg-[#f1f1f1] 
-                            !border border-[rgba(0,0,0,0.4)]" title="view">
-                              <MdOutlineRemoveRedEye  className="text-[rgba(0,0,0,0.7)] text-[20px]"/>
-                            </Button>
-                            <Button className="!w-[35px] !h-[35px] !min-w-[35px] bg-[#f1f1f1] !rounded-full hover:!bg-[#f1f1f1] !border border-[rgba(0,0,0,0.4)]" title="Remove">
-                              <MdDeleteOutline className="text-[rgba(0,0,0,0.7)] text-[20px]"/>
-                            </Button>
-                        </div>
+                    <Button
+                      className="!w-[35px] !h-[35px] !min-w-[35px] bg-[#f1f1f1]
+                             !border border-[rgba(0,0,0,0.4)] !rounded-full hover:!bg-[#f1f1f1]"
+                      title="Edit"
+                    >
+                      <CiEdit className="text-[rgba(0,0,0,0.7)] text-[20px]" />
+                    </Button>
+                    <Button
+                      className="!w-[35px] !h-[35px] !min-w-[35px] bg-[#f1f1f1] !rounded-full hover:!bg-[#f1f1f1] 
+                            !border border-[rgba(0,0,0,0.4)]"
+                      title="view"
+                    >
+                      <MdOutlineRemoveRedEye className="text-[rgba(0,0,0,0.7)] text-[20px]" />
+                    </Button>
+                    <Button
+                      className="!w-[35px] !h-[35px] !min-w-[35px] bg-[#f1f1f1] !rounded-full hover:!bg-[#f1f1f1] !border border-[rgba(0,0,0,0.4)]"
+                      title="Remove"
+                    >
+                      <MdDeleteOutline className="text-[rgba(0,0,0,0.7)] text-[20px]" />
+                    </Button>
+                  </div>
                 </TableCell>
-               
               </TableRow>
-               
-              
-               
             </TableBody>
           </Table>
         </TableContainer>
