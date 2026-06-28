89edf3e9af3495c748ec29df5c966ff929aee338
Aminulhoque01 <aminuhoque53@gmail.com>
2026-06-28 18:23:39 +0600

    veryfi

--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -1,44 +1,48 @@
 import { Link, NavLink } from "react-router";
 
 import logo from "../../assets/logo-light-full.png";
- 
+
 import { IoIosLogIn } from "react-icons/io";
 import { FaRegUserCircle } from "react-icons/fa";
 import Button from "@mui/material/Button";
-import image from "../../assets/verify.png" 
+import image from "../../assets/verify.png";
 import { useRef, useState } from "react";
-
- 
 
 const Verify = () => {
   const [otp, setOtp] = useState(Array(6).fill(""));
-    const inputRefs = useRef([]);
-  
-    const handleChange = (value, index) => {
-      if (/^[0-9]?$/.test(value)) {
-        const newOtp = [...otp];
-        newOtp[index] = value;
-        setOtp(newOtp);
-  
-        // Move to next input
-        if (value && index < 5) {
-          inputRefs.current[index + 1].focus();
-        }
+  const inputRefs = useRef([]);
+
+    const context = useContext(MyContext);
+
+  const handleChange = (value, index) => {
+    if (/^[0-9]?$/.test(value)) {
+      const newOtp = [...otp];
+      newOtp[index] = value;
+      setOtp(newOtp);
+
+      // Move to next input
+      if (value && index < 5) {
+        inputRefs.current[index + 1].focus();
       }
-    };
-  
-    const handleKeyDown = (e, index) => {
-      if (e.key === "Backspace" && !otp[index] && index > 0) {
-        inputRefs.current[index - 1].focus();
-      }
-    };
-  
-    const handleSubmit = () => {
-      const finalOtp = otp.join("");
-      console.log("OTP:", finalOtp);
-      // toast.success(`Your OTP is: ${finalOtp}`);
-    };
-  
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && !otp[index] && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  const handleSubmit = () => {
+    const finalOtp = otp.join("");
+    console.log("OTP:", finalOtp);
+    // toast.success(`Your OTP is: ${finalOtp}`);
+  };
+
+ 
+
+
+
 
   return (
     <section className=" w-full  bg-white  h-[100vh]">
@@ -63,48 +67,47 @@ const Verify = () => {
 
       <div className="loginBox card w-[600px] h-auto mx-auto pt-20 relative z-50">
         <div className="text-center  ">
-          <img src={image} alt="" className="w-[100px] m-auto"/>
+          <img src={image} alt="" className="w-[100px] m-auto" />
         </div>
         <h1 className="text-center text-[35px] font-[800] mt-2">
-           Well come Back !! <br />
+          Well come Back !! <br />
           Please verify your Email
         </h1>
         <br />
-        <p className="text-center text-[15px]">OTP send to &nbsp; <span className="text-blue-500 font-bold">Your email</span></p>
-
-     
+        <p className="text-center text-[15px]">
+          OTP send to &nbsp;{" "}
+          <span className="text-blue-500 font-bold">Your email</span>
+        </p>
 
         <br />
         <div className="text-center flex items-center justify-center flex-col">
           <div className="flex justify-between mb-6 gap-2">
-          {otp.map((value, index) => (
-            <input
-              key={index}
-              ref={(el) => (inputRefs.current[index] = el)}
-              type="text"
-              maxLength="1"
-              value={value}
-              onChange={(e) => handleChange(e.target.value, index)}
-              onKeyDown={(e) => handleKeyDown(e, index)}
-              className="w-12 h-12 text-center text-xl font-semibold border border-gray-300 rounded-lg focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none"
-            />
-          ))}
-        </div>
+            {otp.map((value, index) => (
+              <input
+                key={index}
+                ref={(el) => (inputRefs.current[index] = el)}
+                type="text"
+                maxLength="1"
+                value={value}
+                onChange={(e) => handleChange(e.target.value, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+                className="w-12 h-12 text-center text-xl font-semibold border border-gray-300 rounded-lg focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none"
+              />
+            ))}
+          </div>
 
-        <div className="w-[50%]">
-          <Button
-          onClick={handleSubmit}
-          className="w-full btn-blue text-white py-3 rounded-xl font-semibold org-btn transition"
-        >
-          Verify Otp
-        </Button>
-        </div>
+          <div className="w-[50%]">
+            <Button
+              onClick={handleSubmit}
+              className="w-full btn-blue text-white py-3 rounded-xl font-semibold org-btn transition"
+            >
+              Verify Otp
+            </Button>
+          </div>
         </div>
       </div>
 
       <br />
-
-
     </section>
   );
 };
