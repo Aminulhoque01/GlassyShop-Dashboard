c15b589e5918a1df752d57294ace9e6a693881a2
Aminulhoque01 <aminuhoque53@gmail.com>
2026-07-10 15:45:31 +0600

    address

--- src/pages/Address/AdAddress.jsx
+++ src/pages/Address/AdAddress.jsx
@@ -3,16 +3,41 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import UploadBox from "../../Components/UploadBox/UploadBox";
 import Button from "@mui/material/Button";
 import { FaCloudUploadAlt } from "react-icons/fa";
+import { PhoneInput } from "react-international-phone";
+import React, { useState } from "react";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const AddAddress = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [phone, setPhone] = useState("");
+  const [formFields, setFormFields] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+  });
+
+
+   
+  const noLabelId = React.useId();
+  const [age, setAge] = useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <section className="p-5 bg-gray-50">
       <form className="p-8 py-3 ">
         <div className="max-h-[72vh] overflow-y-scroll pr-4 pt-4">
-          <div className="grid grid-cols-1 mb-3">
-            <div className="col w-[25%]">
+          <div className="grid grid-cols-2 mb-3 gap-4">
+            <div className="col w-[100%]">
               <h3 className="text-[14px] font-[500] mb-1 text-black">
-                Ad Address
+                Address line 1
               </h3>
               <input
                 type="text"
@@ -20,32 +45,79 @@ const AddAddress = () => {
                      focus:border-[rgba(0,0,0,0.4)] rounded-sm p-3 text-sm"
               />
             </div>
+            <div className="col w-[100%]">
+              <h3 className="text-[14px] font-[500] mb-1 text-black">city</h3>
+              <input
+                type="text"
+                className="w-full h-[40px] border border-[rgba(0,0,0,0.2)] bg-[#f3f3f3] focus:outline-none
+                     focus:border-[rgba(0,0,0,0.4)] rounded-sm p-3 text-sm"
+              />
+            </div>
           </div>
-          <h3 className="text-[18px] font-[500] mb-1 text-black">
-            Category Image
-          </h3>
-          <div className="grid grid-cols-7 gap-3">
-            <UploadBox multiple={true} />
-            <div className="uploadBoxWrapper relative">
-              <span className="absolute w-[20px] h-[20px] rounded-full overflow-hidden -top-[10px] -right-[10px] bg-red-500 flex items-center justify-center z-50 cursor-pointer">
-                <IoMdClose className="text-[17px] text-white" />
-              </span>
-
-              <div
-                className="uploadBox p-3 rounded-md overflow-hidden border border-dashed 
-                  border-[rgba(0,0,0,0.3)] h-[150px] w-[100%] bg-gray-100 cursor-pointer hover:bg-gray-200
-                  flex items-center justify-center flex-col relative"
-              >
-                <LazyLoadImage
-                  alt={"image"}
-                  src="https://isomorphic-furyroad.vercel.app/_next/image?url=https%3A%2F%2Fisomorphic-furyroad.s3.amazonaws.com%2Fpublic%2Fproducts%2Fmodern%2F1.webp&w=1920&q=75" // use normal <img> attributes as props
-                  className="w-full h-full object-cover"
-                  effect="blur"
-                  wrapperProps={{
-                    style: { transitionDelay: "1s" },
-                  }}
-                />
-              </div>
+          <div className="grid grid-cols-3 mb-3 gap-4">
+            <div className="col w-[100%]">
+              <h3 className="text-[14px] font-[500] mb-1 text-black">state</h3>
+              <input
+                type="text"
+                className="w-full h-[40px] border border-[rgba(0,0,0,0.2)] bg-[#f3f3f3] focus:outline-none
+                     focus:border-[rgba(0,0,0,0.4)] rounded-sm p-3 text-sm"
+              />
+            </div>
+            <div className="col w-[100%]">
+              <h3 className="text-[14px] font-[500] mb-1 text-black">
+                pin code
+              </h3>
+              <input
+                type="text"
+                className="w-full h-[40px] border border-[rgba(0,0,0,0.2)] bg-[#f3f3f3] focus:outline-none
+                     focus:border-[rgba(0,0,0,0.4)] rounded-sm p-3 text-sm"
+              />
+            </div>
+            <div className="col w-[100%]">
+              <h3 className="text-[14px] font-[500] mb-1 text-black">
+                country
+              </h3>
+              <input
+                type="text"
+                className="w-full h-[40px] border border-[rgba(0,0,0,0.2)] bg-[#f3f3f3] focus:outline-none
+                     focus:border-[rgba(0,0,0,0.4)] rounded-sm p-3 text-sm"
+              />
+            </div>
+            <div className="col w-[100%]">
+              <h3 className="text-[14px] font-[500] mb-1 text-black">mobile</h3>
+              <PhoneInput
+                defaultCountry="bd"
+                value={phone}
+                disabled={isLoading === true ? true : false}
+                onChange={(phone) => {
+                  setPhone(phone);
+                  setFormFields({
+                    mobile: phone,
+                  });
+                }}
+              />
+            </div>
+            <div className="col w-[100%]">
+              <h3 className="text-[14px] font-[500] mb-1 text-black">status</h3>
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <Select
+                  aria-describedby={`${noLabelId}-helper-text`}
+                  value={age}
+                  onChange={handleChange}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Age" }}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+                <FormHelperText id={`${noLabelId}-helper-text`}>
+                  aria-label and helper text
+                </FormHelperText>
+              </FormControl>
             </div>
           </div>
         </div>
